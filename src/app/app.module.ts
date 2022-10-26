c69584398f1d2bad4cbf9573baa2d77344c30929
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-10-26 09:55:55 -0500

    rutas de la Aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+//Modulos Personalizados
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
