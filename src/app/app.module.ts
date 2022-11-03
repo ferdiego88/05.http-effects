77ef4cb6430afb73ba113d811d45ed8731626b43
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-11-03 13:17:19 -0500

    AppReducer, StoreModule y DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { StoreModule } from '@ngrx/store';
+import { appReducers } from './store/app.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { StoreModule } from '@ngrx/store';
     HttpClientModule,
     SharedModule,
     UsuariosModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/reducers/usuarios.reducer.ts
@@ -0,0 +1,39 @@
+import { createReducer, on } from '@ngrx/store';
+import { Usuario } from 'src/app/models/user.model';
+import { cargarUsuarios, cargarUsuariosSuccess,cargarUsuariosError } from '../actions/index';
+
+export interface UsuariosState {
+    users   : Usuario[],
+    loaded  : boolean,
+    loading : boolean,
+    error   : any
+}
+
+export const usuariosinitialState: UsuariosState = {
+    users    : [],
+    loaded   : false,
+    loading  : false,
+    error    : null
+}
+
+export const _usuariosReducer = createReducer(
+    usuariosinitialState,
+    on(cargarUsuarios, state => ({ ...state, loading: true})),
+
+
+    on(cargarUsuariosSuccess, (state,{usuarios}) => ({
+       ...state,
+       loading: false,
+       loaded: true,
+      users: [...usuarios]
+    })),
+
+    on(cargarUsuariosError, (state,{payload}) => ({
+      ...state,
+      loading: false,
+      loaded: false,
+      error: payload,
+      users: []
+    }))
+
+);
